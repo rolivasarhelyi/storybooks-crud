db67384e364f42cfc5ef8074572f26bc555f87a3
rolivasarhelyi <roli.vasarhelyi@gmail.com>
2024-09-08 21:11:15 +0200

    Handle 500 server error

--- routes/index.js
+++ routes/index.js
@@ -17,13 +17,14 @@ router.get('/', ensureGuest, (req, res) => {
 router.get('/dashboard', ensureAuth, async (req, res) => {
   try {
     const stories = await Story.find({ user: req.user.id }).lean();
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    });
   } catch (err) {
     console.log(err);
+    res.render('error/500');
   }
-  console.log(req.user);
-  res.render('dashboard', {
-    name: req.user.firstName,
-  });
 });
 
 module.exports = router;
